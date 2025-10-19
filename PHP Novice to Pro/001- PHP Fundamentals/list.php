dddd77f82f0d48841af4372cd246fbf9f6cb1a75
cyberadityacode <adityadubey793@gmail.com>
2025-10-19 17:04:02 +0530

    list modern usage

--- PHP Novice to Pro/001- PHP Fundamentals/list.php
+++ PHP Novice to Pro/001- PHP Fundamentals/list.php
@@ -30,3 +30,20 @@ $users = [
 foreach($users as list($id,$name)){
     echo "user #$id : $name <br />";
 }
+
+
+// Working with Exploded Strings
+
+$line = "aditya, dubey, 32";
+
+list($first,$second,$age) = explode(',',$line);
+
+echo $age;
+
+// Modern PHP Enhancement: Array Destructuring (PHP 7.1+)
+
+/* list() has a modern shorthand: square bracket destructuring
+(which works identically but is cleaner). */
+
+[$name,$place, $country] = ["Mahakal", "Ujjain", "India"];
+echo $country;
