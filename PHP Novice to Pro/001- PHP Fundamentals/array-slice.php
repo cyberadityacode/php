8da7f46b183a8a4b86030d3f667abe84a8e72cce
cyberadityacode <adityadubey793@gmail.com>
2025-10-20 15:25:04 +0530

    slice

--- PHP Novice to Pro/001- PHP Fundamentals/array-slice.php
+++ PHP Novice to Pro/001- PHP Fundamentals/array-slice.php
@@ -5,4 +5,6 @@ $test_slice = array('a','b','c','d','e');
 echo "<pre>";
 print_r(array_slice($test_slice, 2, count($test_slice))); 
 
+//bottom 2 values slice
 
+print_r(array_slice($test_slice, -2, count($test_slice)));
