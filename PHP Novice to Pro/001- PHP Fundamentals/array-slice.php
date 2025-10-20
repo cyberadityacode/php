f6ca7da4883e1d5ebbcbaae91e13eee13d60d585
cyberadityacode <adityadubey793@gmail.com>
2025-10-20 18:40:15 +0530

    array splice

--- PHP Novice to Pro/001- PHP Fundamentals/array-slice.php
+++ PHP Novice to Pro/001- PHP Fundamentals/array-slice.php
@@ -8,3 +8,11 @@ print_r(array_slice($test_slice, 2, count($test_slice)));
 //bottom 2 values slice
 
 print_r(array_slice($test_slice, -2, count($test_slice)));
+
+// Array splice
+
+$test_splice = array('a','b','c','d','e');
+$spliced_array = array_splice($test_splice, 1, 2, ['x','y']);
+
+print_r($spliced_array);
+print_r($test_splice);
