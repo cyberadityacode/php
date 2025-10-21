37e86732879072b6631c0c4605cef640cff51299
cyberadityacode <adityadubey793@gmail.com>
2025-10-21 17:25:22 +0530

    array value uniques

---
+++ PHP Novice to Pro/001- PHP Fundamentals/array-value-unique.php
@@ -0,0 +1,17 @@
+<?php // phpcs:ignoreFile
+
+$test_val = ['a'=>'red','b'=> 'green','c'=> 'blue','d'=> 'red','e'=> 'yellow'];
+
+echo "<pre>";
+print_r($test_val);
+
+// print just values
+
+$just_values = array_values($test_val);
+
+print_r($just_values);
+
+// print uniques
+
+$unique_values = array_unique($test_val);
+print_r($unique_values);
