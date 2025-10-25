aae675ac665ddbffca4f0cc05363ef0afa68b233
cyberadityacode <adityadubey793@gmail.com>
2025-10-26 04:25:22 +0530

    string search

---
+++ PHP Novice to Pro/001- PHP Fundamentals/stringsearch.php
@@ -0,0 +1,13 @@
+<?php // phpcs:ignoreFile
+
+$string_test = "aditya dubey is greatest of all time";
+
+echo strstr($string_test,"dubey") . "<br />";
+
+echo strchr($string_test, "dubey") . "<br />";
+
+// string reverse
+
+echo strrev($string_test). "<br />";
+
+echo str_shuffle($string_test);
