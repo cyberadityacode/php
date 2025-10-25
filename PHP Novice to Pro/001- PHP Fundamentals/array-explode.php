43a6fb176c9fd4a84aeccd61eb64a308cf3fe93b
cyberadityacode <adityadubey793@gmail.com>
2025-10-25 18:21:15 +0530

    array explode basic

---
+++ PHP Novice to Pro/001- PHP Fundamentals/array-explode.php
@@ -0,0 +1,18 @@
+<?php // phpcs:ignoreFile
+
+$stringTest = "hello how are you";
+
+// explode(separator, string, limit);
+
+$arrayTest = explode(' ',$stringTest);
+echo "<pre>";
+print_r($arrayTest);
+
+// find longest word in arrayTest.
+
+usort($arrayTest, function($a,$b){
+    return strlen($b) - strlen($a);
+});
+echo "Longest word is $arrayTest[0]";
+
+// one line function using reduce
