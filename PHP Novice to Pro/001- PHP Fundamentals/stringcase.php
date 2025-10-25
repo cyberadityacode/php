5a746302f2e41fbcb6e4f0244740c141799b5b24
cyberadityacode <adityadubey793@gmail.com>
2025-10-26 03:45:31 +0530

    string case functions

---
+++ PHP Novice to Pro/001- PHP Fundamentals/stringcase.php
@@ -0,0 +1,14 @@
+<?php // phpcs:ignoreFile
+
+$string_test = "aditya dubey";
+
+echo strtoupper($string_test) . "<br />"; // all upper
+
+echo strtolower($string_test). "<br />"; // all lower
+
+echo ucfirst($string_test). "<br />"; // first letter upper
+
+echo lcfirst($string_test).  "<br />"; //first letter lower
+
+echo ucwords($string_test); // first letter of every word upper
+
