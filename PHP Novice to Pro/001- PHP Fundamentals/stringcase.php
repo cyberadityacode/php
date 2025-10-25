510cef54d6694aafe2dccedc1d26ed292e687516
cyberadityacode <adityadubey793@gmail.com>
2025-10-26 03:53:43 +0530

    string case

--- PHP Novice to Pro/001- PHP Fundamentals/stringcase.php
+++ PHP Novice to Pro/001- PHP Fundamentals/stringcase.php
@@ -10,5 +10,14 @@ echo ucfirst($string_test). "<br />"; // first letter upper
 
 echo lcfirst($string_test).  "<br />"; //first letter lower
 
-echo ucwords($string_test); // first letter of every word upper
+echo ucwords($string_test). "<br />"; // first letter of every word upper
+
+echo strlen($string_test). "<br />"; // length of string
+
+echo str_word_count($string_test); // count total words in a string
+
+
+echo "<br />";
+
+echo substr_count($string_test, 'd'); // counts the number of iterations for a particular sub string
 
