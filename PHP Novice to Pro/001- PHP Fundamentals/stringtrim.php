a01fa67ef517aa5a8c925ff172fafbd7e8aa496d
cyberadityacode <adityadubey793@gmail.com>
2025-10-26 04:39:23 +0530

    string trim

---
+++ PHP Novice to Pro/001- PHP Fundamentals/stringtrim.php
@@ -0,0 +1,6 @@
+<?php // phpcs:ignoreFile
+
+$string_test = " aditya dubey ";
+echo strlen($string_test) . "<br />"; //14 with spaces
+echo strlen(trim($string_test)) ."<br />";  // 12 without spaces
+
